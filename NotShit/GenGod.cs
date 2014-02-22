39d3e9fd68a1744f3abd7a41eaf40fab72d283e7
Martinho Fernandes <martinho.fernandes@gmail.com>
2014-02-22 19:26:37 +0100

    Random Number God.

---
+++ NotShit/GenGod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace NotShit
+{
+    public static class GenGod
+    {
+        private static Random generator = new Random();
+
+        public static void Reseed(int seed)
+        {
+            generator = new Random(seed);
+        }
+
+        public static int GenOne(int lowerInclusive, int upperExclusive)
+        {
+            return generator.Next(lowerInclusive, upperExclusive);
+        }
+
+        public static int RollDice(int number, int sides)
+        {
+            return Enumerable.Range(0, number).Sum(_ => RollDie(sides));
+        }
+
+        public static int RollDie(int sides)
+        {
+            return generator.Next(0, sides) + 1;
+        }
+    }
+}
